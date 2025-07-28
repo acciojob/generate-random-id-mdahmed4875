6bcee0300243a602bf321e4a7020bf1b23d05255
mdahmed4875 <140884439+mdahmed4875@users.noreply.github.com>
2025-07-28 08:28:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function makeid(l) {
   // write your code here
+	let res=""
+	let char_list="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+	for(let i=0;i<l;i++){
+		res+=char_list.charAt(
+			Math.floor(
+			Math.random()*char_list.length;
+				)
+		)
+	}
+	return res;
 }
 
 // Do not change the code below.
